7dc1a3640eb82202b5ec14c0213f9d307f6034c0
ALEJANDRO ANTONIO AGUILAR PAZ <agpaz90@gmail.com>
2025-04-18 12:42:22 -0500

    Second round of excercises (#1)

---
+++ Tema_B_Ciclos/02_TemaB_digitos.c
@@ -0,0 +1,14 @@
+// Contador de dígitos: Pide un número entero y cuenta cuántos dígitos tiene usando un ciclo `while`.
+
+// *Tabla de multiplicar**: Solicita un número y muestra su tabla de multiplicar 
+// del 1 al 10 usando un `while`.
+
+#include <stdio.h>
+
+int main ()
+{
+
+    return 0;
+}
+
+
